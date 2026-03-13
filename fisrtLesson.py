fd862f1fdeda9bfa79dccfbf6c68f27abeb88207
Paradox2141 <usarikurk@gmail.com>
2026-03-13 18:51:58 +0100

    haftanin gunlerini yazdir

---
+++ fisrtLesson.py
@@ -0,0 +1,26 @@
+print("Hello, World!")
+# define a function to greet the user
+def greet(name):
+    print(f"Hello, {name}!")
+# call the function with a namegreet("Alice")
+greet("Ufuk")    
+
+# 1'den 100'e kadar asal sayıları yazdıran fonksiyon
+def print_primes_1_to_100():
+    for num in range(2, 101):
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(num)
+
+# Fonksiyonu çağır
+print_primes_1_to_100()
+
+# haftanın gonlerini yazdıran fonksiyon
+def print_weekdays():
+    weekdays = ["Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar"]
+    for day in weekdays:
+        print(day)
